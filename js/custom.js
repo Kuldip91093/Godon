041458445cd5c47618ef7d168146d103bd76b556
SanjayBarad <baradsanjay2018@gmail.com>
2022-03-09 02:41:35 +0530

    Add Responsive Tabs | Sanjay

--- js/custom.js
+++ js/custom.js
@@ -60,23 +60,48 @@ $(document).ready(function(){
 
 
 // CUSTOM TABS
+// $(document).ready(function(){
+// 	// On Page Load Active Tab
+// 	var currActiveTab = $('.custom_tabs .custom_tabs_nav a.tab_nav_active').attr('data-tag');
+// 	console.log(currActiveTab);
+// 	$('.custom_tabs .tabs_content_item').hide();
+// 	$('.custom_tabs .tabs_content_item.' + currActiveTab).fadeIn();
+// 	$('.custom_tabs .tabs_content_item.' + currActiveTab).addClass('tab_content_active');
+
+// 	// On Click Change Tab
+// 	$('.custom_tabs .custom_tabs_nav a').click(function(){
+// 		$(this).parents('.custom_tabs').find('.custom_tabs_nav a').removeClass('tab_nav_active');
+// 		$(this).addClass('tab_nav_active');
+
+// 		var currActiveTab = $(this).attr('data-tag');
+// 		$(this).parents('.custom_tabs').find('.tabs_content_item').hide();
+// 		$(this).parents('.custom_tabs').find('.tabs_content_item').removeClass('tab_content_active');
+// 		$(this).parents('.custom_tabs').find('.tabs_content_item.' + currActiveTab).fadeIn();
+// 		$(this).parents('.custom_tabs').find('.tabs_content_item.' + currActiveTab).addClass('tab_content_active');
+// 	});
+// });
+
+// Responsive Tabs
 $(document).ready(function(){
-	// On Page Load Active Tab
-	var currActiveTab = $('.custom_tabs .custom_tabs_nav a.tab_nav_active').attr('data-tag');
-	console.log(currActiveTab);
-	$('.custom_tabs .tabs_content_item').hide();
-	$('.custom_tabs .tabs_content_item.' + currActiveTab).show();
-	$('.custom_tabs .tabs_content_item.' + currActiveTab).addClass('tab_content_active');
 
-	// On Click Change Tab
-	$('.custom_tabs .custom_tabs_nav a').click(function(){
-		$(this).parents('.custom_tabs').find('.custom_tabs_nav a').removeClass('tab_nav_active');
-		$(this).addClass('tab_nav_active');
-
-		var currActiveTab = $(this).attr('data-tag');
-		$(this).parents('.custom_tabs').find('.tabs_content_item').hide();
-		$(this).parents('.custom_tabs').find('.tabs_content_item').removeClass('tab_content_active');
-		$(this).parents('.custom_tabs').find('.tabs_content_item.' + currActiveTab).show();
-		$(this).parents('.custom_tabs').find('.tabs_content_item.' + currActiveTab).addClass('tab_content_active');
+	$('#re_purchase').responsiveTabs({
+		startCollapsed: 'accordion',
+		active: 1,
+		collapsible: true,
+		animation: 'slide'
 	});
+
+	$('#re_sale').responsiveTabs({
+		startCollapsed: 'accordion',
+		active: 1,
+		collapsible: true,
+		animation: 'slide'
+	});
+
+	setTimeout(function(){
+		var triggerEl = $('.active_tab_on_load').prev('.r-tabs-accordion-title').find('.r-tabs-anchor')
+		triggerEl.trigger('click');
+		console.log(triggerEl)
+	},100);
+
 });
